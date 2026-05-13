4978694fa3a31cf280415dad5c125964de5e60d7
PURUSHOTHAM-spr <purushothamshagamreddy464@gmail.com>
2026-05-13 11:31:54 +0530

    mern

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(cookieParser());
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: 'https://online-learning-platform-livid.vercel.app/',
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
     allowedHeaders: ["Content-Type", "Authorization"]
